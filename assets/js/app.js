1faf23e7a45dea74181a3d0da9033506c9b4d5df
Stephen Ruan <Stephen@Stephens-MacBook-Pro.local>
2018-05-01 00:42:46 -0700

    smooth_scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 	'use strict'
-	// add active to li links //
+	// add active to li links /
+
     // $('.nav-link').click(function (e) {
     //     $('.subTitle').addClass('active');
     // });    })
 
     // hide navbar //
-    $(document).scrollTop(0);
+    $('html').scrollTop(0);
+
     $('.navbar').hide();
 
+  
     // function to show navbar //
     $(window).scroll(function(){                          
             if ($(this).scrollTop() > 805) {
